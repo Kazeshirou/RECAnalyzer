5bccfb8c6ffbf7eecd172d991bf75635f2c37f66
Natalie Zharova <zharntl@yandex.ru>
2021-06-22 14:36:47 +0300

    fix: исправлена работа априори

--- src/common/multimodal_communication/assotiation_mining/apriori.cpp
+++ src/common/multimodal_communication/assotiation_mining/apriori.cpp
@@ -1,7 +1,6 @@
 #include "apriori.hpp"
 
 #include <functional>
-#include <map>
 #include <vector>
 
 namespace mc::assotiation_mining::algorithm {
@@ -9,48 +8,55 @@ namespace mc::assotiation_mining::algorithm {
 case_t apriori_sets(const apriori_settings_t& settings,
                     const case_t&             transactions) {
     struct node_t {
-        size_t                   value{-1ull};
-        std::vector<entry_t*>    transactions;
-        std::map<size_t, node_t> children;
+        node_t(size_t size) : children(size, nullptr) {}
+        ~node_t() {
+            for (auto child : children) {
+                if (child) {
+                    delete child;
+                }
+            }
+        }
+        size_t               value{0};
+        std::vector<node_t*> children;
     };
     size_t size = transactions[0]->size();
-    node_t hash;
+    node_t hash(size);
+    hash.value = transactions.size();
     case_t sets;
 
     for (auto& transaction : transactions) {
-        std::vector<node_t*> current_nodes;
-        hash.transactions.push_back(transaction);
-        current_nodes.push_back(&hash);
+        node_t* node = &hash;
         for (size_t i{0}; i < size; i++) {
             if (!transaction->check_bit(i)) {
                 continue;
             }
-            size_t current_size = current_nodes.size();
-            for (size_t j{0}; j < current_size; j++) {
-                if (current_nodes[j]->children[i].value == -1ull) {
-                    current_nodes[j]->children[i].value = i;
-                }
-                current_nodes[j]->children[i].transactions.push_back(
-                    transaction);
-                current_nodes.push_back(&(current_nodes[j]->children[i]));
+
+            if (!node->children[i]) {
+                node->children[i] = new node_t(size);
             }
+
+            node = node->children[i];
+            ++node->value;
         }
     }
 
-    std::function<void(node_t&, Bit_mask)> add_to_sets =
-        [&](node_t& current_node, Bit_mask current_mask) {
-            double support =
-                current_node.transactions.size() / double(transactions.size());
+    std::function<void(const node_t&, Bit_mask)> add_to_sets =
+        [&](const node_t& current_node, Bit_mask current_mask) {
+            double support = current_node.value / double(transactions.size());
             if (support < settings.min_support) {
                 return;
             }
             if (support <= settings.max_support) {
                 sets.push_back(new set_t{current_mask, support});
             }
-            for (auto& child : current_node.children) {
+            for (size_t i{0}; i < size; i++) {
+                if (!current_node.children[i]) {
+                    continue;
+                }
+
                 Bit_mask child_mask = current_mask;
-                child_mask.set_bit(child.first);
-                add_to_sets(child.second, child_mask);
+                child_mask.set_bit(i);
+                add_to_sets(*current_node.children[i], child_mask);
             }
         };
 
